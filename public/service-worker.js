3c44900b01ffab3d5292c6f927325498981242c0
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-07-22 13:08:22 -0500

    created service-worker file

---
+++ public/service-worker.js
@@ -0,0 +1,61 @@
+// create a service worker that allows the user to utilize the PWA while they are
+// offline.  It will cache the data into indexDb as well as caching the regular
+// pages used
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./public/index.html",
+    "./public/assets/js/index.js",
+    "./public/assets/js/db.js",
+    "./public/images/icons/icon-192x192.png",
+    "./public/images/icons/icon-512x512.png"
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('installing cache : ' + CACHE_NAME)
+                return cache.addAll(FILES_TO_CACHE)
+            })
+    )
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches
+            .match(e.request)
+            .then(function (request) {
+                if (request) {
+                    console.log('responding with cache : ' + e.request.url)
+                    return request;
+                } else {
+                    console.log('file is not cached, fetching : ' + e.request.url)
+                    return fetch(e.request)
+                }
+            })
+    )
+});
